345e92df7462a17dc5c9d89111a57a9c5f63aee3
Greg Johnston <gregmj.sco@gmail.com>
2025-09-14 17:51:46 +0900

    Sanity setup, add product & category schema

---
+++ schemaTypes/product.ts
@@ -0,0 +1,67 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    }),
+    defineField({
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{type: 'image'}],
+      options: {layout: 'grid'},
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'inStock',
+      title: 'In Stock',
+      type: 'boolean',
+      initialValue: true,
+    }),
+    defineField({
+      name: 'variants',
+      title: 'Variants',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          name: 'variant',
+          fields: [
+            {name: 'name', title: 'Name', type: 'string'},
+            {name: 'price', title: 'Price', type: 'number'},
+          ],
+        },
+      ],
+    }),
+  ],
+})
